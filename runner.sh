bcba641650e2545782389ce411bdb20041ada0dc
tm <torrez.mn@gmail.com>
2024-06-14 23:22:31 -0400

    UPDATED - {2024-06-14} - {23:22:28}

--- runner.sh
+++ runner.sh
@@ -1,7 +1,27 @@
+#!/bin/bash
 
-echo "Running Stock"
+
+
+run_stock(){
+echo "Running Stock with the value $1"
 python3 /home/torrezmn/Documentos/Py_Prices/src/stock/stock_main.py $1
-echo "Running Super6"
-python3 /home/torrezmn/Documentos/Py_Prices/src/super6/super6_main.py $2
+}
 
+run_super6(){
+echo "Running Super6 with the value $1"
+python3 /home/torrezmn/Documentos/Py_Prices/src/super6/super6_main.py $1
+}
+
+
+gen_random() {
+  return $((1 + $RANDOM % 100))
+}
+
+
+for i in {0..8}; do  # Use curly braces for inclusive range
+    gen_random
+    run_stock $?
+    gen_random
+    run_super6 $?
+done
 
